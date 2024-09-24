e481a15820db850ade2d32c474a551242b1ff38b
lingk4231075 <strbryjam1220@gmail.com>
2024-09-24 09:53:40 +0900

    프린트 통해서 정상 루프 구조가 잘 실행됨.

--- character_grass.py
+++ character_grass.py
@@ -1,11 +1,12 @@
 from pico2d import *
 
 def run_rectangle():
+    print('RECTANGLE')
     pass
 
 def run_circle():
+    print('CIRCLE')
     pass
-
 
 
 open_canvas()
@@ -13,11 +14,9 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
-
-
-
 while True:
-    run_rectangle()
+
     run_circle()
+    run_rectangle()
 
 close_canvas()
