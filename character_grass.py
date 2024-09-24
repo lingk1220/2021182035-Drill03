69ac20bf55c67b14304e31abba7b1aa8e89309c4
lingk4231075 <strbryjam1220@gmail.com>
2024-09-24 10:37:39 +0900

    circle 테스트 중단

--- character_grass.py
+++ character_grass.py
@@ -7,8 +7,29 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_top():
+    print('TOP')
+    pass
+
+def run_right():
+    print('RIGHT')
+    pass
+
+def run_bottom():
+    print('BOTTOM')
+    pass
+
+def run_left():
+    print('LEFT')
+    pass
+
 def run_rectangle():
     print('RECTANGLE')
+
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
     pass
 
 def run_circle():
@@ -29,7 +50,7 @@ def run_circle():
 
 while True:
     
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
     
